cf20e1334c82834e9115d72b573e50d9d87fe47b
Hack Reactor Student <student@hackreactor.com>
2020-02-11 10:37:19 -0800

    Add hulk dancer and stage background

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,7 +4,6 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
   // return this;
 };
 
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
@@ -22,11 +23,14 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+
+
     $('body').append(dancer.$node);
   });
 });
